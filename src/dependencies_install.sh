3c2e47feb461cc66d2c18508217be3d253cd15a8
Shayegan Omidshafiei <shayegan@mit.edu>
2017-11-29 20:37:38 -0500

    Update dependencies_install.sh

--- src/dependencies_install.sh
+++ src/dependencies_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pip install python_speech_features seaborn numpy scipy matplotlib ipython jupyter pandas sympy nose opencv-python
+sudo pip install python_speech_features seaborn numpy scipy matplotlib  pandas sympy nose opencv-python
 
 

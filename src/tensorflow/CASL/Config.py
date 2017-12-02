27d63195835dfc4f83566baa356ec0d3b8a2ed66
dongki <dkim930122@ttic.edu>
2017-12-02 16:23:43 -0500

    Config file cleaned

--- src/tensorflow/CASL/Config.py
+++ src/tensorflow/CASL/Config.py
@@ -30,19 +30,20 @@ class Config:
     #########################################################################
     # GENERAL PARAMETERS
     CODE_DIR                        =  os.path.dirname(__file__)
-    game_doorpuzzle, game_minecraft = range(2) # Initialize game types as enum
+    game_doorpuzzle, 
+    game_minecraft,
+    game_ale                        = range(3)       # Initialize game types as enum
     GAME_CHOICE                     = game_minecraft
-    USE_AUDIO                       = True # Enable audio input
-    TRAIN_MODELS                    = True # Enable to train
-    DEBUG                           = False # Enable debug (prints more information for debugging purpose)
-    LOAD_EPISODE                    = 0 # If 0, the latest checkpoint is loaded
-    LOAD_CHECKPOINT                 = False # Load old models. Throws if the model doesn't exist
-    PLAY_MODE                       = False # Enable to see the trained agent in action (for testing)
-    TRANSFER_MODE                   = False # Enable to see the trained agent in action (for testing)
-    if TRANSFER_MODE:
+    USE_AUDIO                       = True           # Enable audio input
+    TRAIN_MODELS                    = True           # Enable to train
+    LOAD_EPISODE                    = 0              # If 0, the latest checkpoint is loaded
+    LOAD_CHECKPOINT                 = False          # Load old models. Throws if the model doesn't exist
+    PLAY_MODE                       = False          # Enable to see the trained agent in action (for testing)
+    TRANSFER_MODE                   = False          # Enable to see the trained agent in action (for testing)
+    if TRANSFER_MODE:                                # Transfer learning for game_doorpuzzle
         LOAD_CHECKPOINT             = True
         TRAIN_MODELS                = False
-    VIS_FREQUENCY                   = 500 # Unit in episode
+    VIS_FREQUENCY                   = 500            # Enable Vis during trainig every VIS_FREQUENCY
     if LOAD_CHECKPOINT:
         LOGDIR          = os.path.join('tmp-logs','2017-10-28_23h-08m-24s')
     else:
@@ -52,7 +53,7 @@ class Config:
     # MAZEWORLD GAME PARAMETER
     if GAME_CHOICE == game_doorpuzzle:
         REWARD_MIN     = -1    # Reward Clipping
-        REWARD_MAX     = 1
+        REWARD_MAX     = 1     # Reward Clipping
         ENV_ROW        = 5     # Number of patch in row direction
         ENV_COL        = 5     # Number of patch in col direction
         PIXEL_SIZE     = 20    # Pixel size for each patch
@@ -62,97 +63,97 @@ class Config:
         LISTEN_RANGE   = 1.5   # Distance needed to listen to a target
         HARD_MODE      = False # Gate and target positions become random
         if TRANSFER_MODE:
-            HARD_MODE  = True
-        SIMPLE_RENDER  = True
+            HARD_MODE  = True  # If TRANSFER_MODE, set harder environment
+        SIMPLE_RENDER  = True  # Option whether to show simple vis or more complex vis with characters, items, etc
 
     # SDWORLD GAME PARAMETER
     elif GAME_CHOICE == game_minecraft:
-        REWARD_MIN     = -100 # Reward Clipping
-        REWARD_MAX     = 100
-        ENV_ROW        = 5 # Number of patch in row direction
-        ENV_COL        = 5 # Number of patch in col direction
-        PIXEL_SIZE     = 20 # Pixel size for each patch
-        MAX_ITER       = 30 # Max iteration (time limit)
-        LISTEN_RANGE   = 1.5 # Distance needed to listen to a target
+        REWARD_MIN     = -100  # Reward Clipping
+        REWARD_MAX     = 100   # Reward Clipping
+        ENV_ROW        = 5     # Number of patch in row direction
+        ENV_COL        = 5     # Number of patch in col direction
+        PIXEL_SIZE     = 20    # Pixel size for each patch
+        MAX_ITER       = 30    # Max iteration (time limit)
+        LISTEN_RANGE   = 1.5   # Distance needed to listen to a target
         TIMER_DURATION = 0.001 # In second visualization time for each step
-        SIMPLE_RENDER  = False
+        SIMPLE_RENDER  = False # Option whether to show simple vis or more complex vis with characters, items, etc
 
     #########################################################################
     # NET ARCHITECTURE
     # Possible NET_ARCH: 'Net_3C_A_K_XL_F', 'Net_3C_XL_A_F', 'Net_3C_F'
     NET_ARCH                       = 'Net_3C_A_K_XL_F' # Neural net architecture. Any from the 'models' folder can be selected.
-    USE_OPTIONS                    = True
-    USE_RNN                        = True
-    USE_ATTENTION                  = True
-    attn_temporal, attn_multimodal = range(2) # For now we support either one or the other
+    USE_OPTIONS                    = True              # If True, learn option policy. If False, learn (primitive) action policies
+    USE_RNN                        = True              # Enable to tse LSTM in neural net architecture
+    USE_ATTENTION                  = True              # Enable to use attention for crossmodal
+    attn_temporal, attn_multimodal = range(2)          # NOTE This code only works for attn_multimodal
     ATTN_TYPE                      = attn_multimodal
 
     if USE_RNN:
-        NUM_LAYERS_PER_LSTM        = 1 # NOTE more than 1 layer used to work, but no longer.
-        NCELLS                     = 64
+        NUM_LAYERS_PER_LSTM        = 1   # NOTE not fully implemented for more than 1 layer
+        NCELLS                     = 128
         STACKED_FRAMES             = 1
         if ATTN_TYPE == attn_multimodal:
-            NMODES                 = 2 # TODO not fully implemented >2 case yet, but introduced as variable to avoid magic numbers in code
+            NMODES                 = 2   # NOTE not fully implemented >2 case yet
         elif ATTN_TYPE == attn_temporal: 
-            ATTN_TEMPORAL_WINDOW   = 3 # Temporal window of previous attention states to pay attention to
+            ATTN_TEMPORAL_WINDOW   = 3   # Temporal window of previous attention states to pay attention to
             ATTN_STATE_NCELLS      = 128 # Is independent of Config.NCELLS
     else:
         STACKED_FRAMES             = 1
 
     if USE_OPTIONS:
         NUM_OPTIONS                = 3
-        COST_DELIB_START           = 0.000 # For deliberation cost
+        COST_DELIB_START           = 0.000  # For deliberation cost
         COST_DELIB_END             = 0.0002 # For deliberation cost
         COST_MARGIN                = 0.0002
-        OPTION_EPSILON_START       = 0.15  # For epsilon greedy in terms of options
-        OPTION_EPSILON_END         = 0.15  # For epsilon greedy in terms of options
+        OPTION_EPSILON_START       = 0.15   # For epsilon greedy in terms of options
+        OPTION_EPSILON_END         = 0.15   # For epsilon greedy in terms of options
 
     #########################################################################
     # NUMBER OF AGENTS, PREDICTORS, TRAINERS, AND OTHER SYSTEM SETTINGS
     # IF THE DYNAMIC CONFIG IS ON, THESE ARE THE INITIAL VALUES
-    AGENTS                        = 32 # Number of Agents
-    PREDICTORS                    = 4 # Number of Predictors
-    TRAINERS                      = 4 # Number of Trainers
+    AGENTS                        = 32       # Number of Agents
+    PREDICTORS                    = 4        # Number of Predictors
+    TRAINERS                      = 4        # Number of Trainers
     DEVICE                        = '/gpu:0' # Device
-    DYNAMIC_SETTINGS              = False # Enable the dynamic adjustment (+ waiting time to start it)
+    DYNAMIC_SETTINGS              = False    # Enable the dynamic adjustment (+ waiting time to start it)
     DYNAMIC_SETTINGS_STEP_WAIT    = 20
     DYNAMIC_SETTINGS_INITIAL_WAIT = 10
 
     #########################################################################
     # ALGORITHM PARAMETER
-    DISCOUNT                = 0.99 # Discount factor
-    TIME_MAX                = 10 # Tmax
-    MAX_QUEUE_SIZE          = 100 # Max size of the queue
-    PREDICTION_BATCH_SIZE   = 128 # I think this just has to be > AGENTS
-    IMAGE_WIDTH             = 84 # Input of the DNN
-    IMAGE_HEIGHT            = 84
+    DISCOUNT                = 0.99    # Discount factor
+    TIME_MAX                = 10      # Tmax
+    MAX_QUEUE_SIZE          = 100     # Max size of the queue
+    PREDICTION_BATCH_SIZE   = 128     # I think this just has to be > AGENTS
+    IMAGE_WIDTH             = 84      # Input size of the DNN
+    IMAGE_HEIGHT            = 84      # Input size of the DNN
     EPISODES                = 4000000 # Total number of episodes and annealing frequency
     ANNEALING_EPISODE_COUNT = 400000
 
     # OPTIMIZER PARAMETERS
     OPT_RMSPROP, OPT_ADAM   = range(2) # Initialize optimizer types as enum
     OPTIMIZER               = OPT_ADAM # Optimizer choice 
-    LEARNING_RATE_START     = 1e-4 # Learning rate
-    LEARNING_RATE_END       = 1e-4
+    LEARNING_RATE_START     = 1e-4     # Learning rate
+    LEARNING_RATE_END       = 1e-4     # Learning rate
     RMSPROP_DECAY           = 0.99
     RMSPROP_MOMENTUM        = 0.0
     RMSPROP_EPSILON         = 0.1
-    BETA_START              = 0.01 # Entropy regularization hyper-parameter
+    BETA_START              = 0.01     # Entropy regularization hyper-parameter
     BETA_END                = 0.01
-    BETA_ATTENTION          = 1e-3 # Entropy regularization for attention modality term
-    USE_GRAD_CLIP           = False # Gradient clipping
+    BETA_ATTENTION          = 1e-3     # Entropy regularization for attention modality term
+    USE_GRAD_CLIP           = False    # Enable to use gradient clipping
     GRAD_CLIP_NORM          = 40.0 
-    LOG_EPSILON             = 1e-3 # Epsilon (regularize policy lag)
-    TRAINING_MIN_BATCH_SIZE = 40 # Training min batch size - increasing the batch size increases the stability of the algorithm, but make learning slower
+    LOG_EPSILON             = 1e-3     # Epsilon (regularize policy lag)
+    TRAINING_MIN_BATCH_SIZE = 40       # Training min batch size - increasing the batch size increases the stability of the algorithm, but make learning slower
 
     #########################################################################
     # LOG AND SAVE
-    TENSORBOARD                  = True # Enable TensorBoard
-    TENSORBOARD_UPDATE_FREQUENCY = 50 # Update TensorBoard every X training steps
-    SAVE_MODELS                  = True # Enable to save models every SAVE_FREQUENCY episodes
-    SAVE_FREQUENCY               = 100 # Save every SAVE_FREQUENCY episodes
-    PRINT_STATS_FREQUENCY        = 1 # Print stats every PRINT_STATS_FREQUENCY episodes
-    STAT_ROLLING_MEAN_WINDOW     = 1000 # The window to average stats
+    TENSORBOARD                  = True         # Enable TensorBoard
+    TENSORBOARD_UPDATE_FREQUENCY = 50           # Update TensorBoard every X training steps
+    SAVE_MODELS                  = True         # Enable to save models every SAVE_FREQUENCY episodes
+    SAVE_FREQUENCY               = 100          # Save every SAVE_FREQUENCY episodes
+    PRINT_STATS_FREQUENCY        = 1            # Print stats every PRINT_STATS_FREQUENCY episodes
+    STAT_ROLLING_MEAN_WINDOW     = 1000         # The window to average stats
     RESULTS_FILENAME             = 'results.txt'# Results filename
 
     #########################################################################

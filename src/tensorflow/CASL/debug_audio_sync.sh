db30990553ebf1277b3f2dfe1f38c7f70ecda17e
Dong Ki Kim <dkim930122@ttic.edu>
2017-12-02 18:32:07 -0500

    Removed unnecessary files

--- src/tensorflow/CASL/debug_audio_sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# A script to generate an ALE video with FFMpeg, *nix systems. 
-
-# Attempt to use ffmpeg. If this fails, use avconv (fix for Ubuntu 14.04). 
-{
-    ffmpeg -r 60 -i debug/17263/%06d.png -i debug/17263/sound.wav -f mov -c:a mp3 -c:v libx264 agent.mov
-} 

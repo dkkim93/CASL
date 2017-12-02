5a641a7bccf0c8f2aec8e89bbb61fb8d9ee29183
dongki <dkim930122@ttic.edu>
2017-12-02 14:12:16 -0500

    Minor format fix

--- src/dependencies_install.sh
+++ src/dependencies_install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo pip install python_speech_features seaborn numpy scipy matplotlib  pandas sympy nose opencv-python
-
-
+sudo pip install python_speech_features seaborn numpy scipy matplotlib pandas sympy nose opencv-python

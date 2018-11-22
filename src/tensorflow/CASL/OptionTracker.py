2056f40a680b7b625ce1f941d3d73f9ce2ec5d20
dongki <dkim930122@ttic.edu>
2018-11-21 22:30:14 -0500

    * pep8 style update
    * reset tracker added

--- src/tensorflow/CASL/OptionTracker.py
+++ src/tensorflow/CASL/OptionTracker.py
@@ -1,7 +1,7 @@
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 from Config import Config
+
 
 class OptionTracker():
     """
@@ -9,7 +9,7 @@ class OptionTracker():
         Useful for analyzing learned options
     """
     def __init__(self):
-        self.img_traj_tracker        = np.zeros((Config.ENV_COL, Config.ENV_ROW, Config.NUM_OPTIONS))
+        self.img_traj_tracker = np.zeros((Config.ENV_COL, Config.ENV_ROW, Config.NUM_OPTIONS))
         self.img_option_term_tracker = np.zeros((Config.ENV_COL, Config.ENV_ROW, Config.NUM_OPTIONS))
 
         self._init_axes()
@@ -21,24 +21,28 @@ class OptionTracker():
         self.axes = {}
         for i_option in range(Config.NUM_OPTIONS):
             key = str(i_option) + '_0'
-            ax = plt.subplot2grid((2,Config.NUM_OPTIONS), (0,i_option))
+            ax = plt.subplot2grid((2, Config.NUM_OPTIONS), (0, i_option))
             ax.set_title("Option " + str(i_option) + " traj")
             self.axes[key] = ax
 
             key = str(i_option) + '_1'
-            ax = plt.subplot2grid((2,Config.NUM_OPTIONS), (1,i_option))
+            ax = plt.subplot2grid((2, Config.NUM_OPTIONS), (1, i_option))
             ax.set_title("Option " + str(i_option) + " term")
             self.axes[key] = ax
 
         self.artists = {}
         for i_option in range(Config.NUM_OPTIONS):
             key = str(i_option) + '_0'
-            img_artist = self.axes[key].imshow(np.zeros((5,5)), cmap = 'copper')
+            img_artist = self.axes[key].imshow(np.zeros((5, 5)), cmap="copper")
             self.artists[key] = img_artist
 
             key = str(i_option) + '_1'
-            img_artist = self.axes[key].imshow(np.zeros((5,5)), cmap = 'copper')
+            img_artist = self.axes[key].imshow(np.zeros((5, 5)), cmap="copper")
             self.artists[key] = img_artist
+
+    def _reset_tracker(self):
+        self.img_traj_tracker = np.zeros((Config.ENV_COL, Config.ENV_ROW, Config.NUM_OPTIONS))
+        self.img_option_term_tracker = np.zeros((Config.ENV_COL, Config.ENV_ROW, Config.NUM_OPTIONS))
 
     def _update_tracker(self, agt_loc, i_option, option_term):
         self.img_traj_tracker[agt_loc[0], agt_loc[1], i_option] += 1
